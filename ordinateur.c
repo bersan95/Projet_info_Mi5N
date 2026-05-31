6d3a15c584733c8699224bd910b6348fa0580822
Bervio <sayhanbersan0@gmail.com>
2026-05-31 13:51:05 +0200

    Update ordinateur.c

--- ordinateur.c
+++ ordinateur.c
@@ -4,7 +4,13 @@
 // 1. Fonction qui décide à la place du joueur (Renvoie 1 pour piocher, 2 pour s'arrêter)
 int faire_choix_ordinateur(Joueur bot) {
     
-//2. On va lui donner une stratégie simple : il pioche s'il a moins de 4 cartes, et il s'arrête s'il a 4 cartes ou plus (pour ne pas prendre trop de risques).
+//2. On va lui donner une stratégie simple : il pioche s'il a moins de 4 cartes, et il s'arrête s'il a 4 cartes ou plus (pour ne pas prendre trop de risques) mais pioche s'il possède une carte 2nd chanche 
+
+if (bot.a_2nd_chance == true) {
+    printf(" 🤖 L'ordinateur (%s) se sent intouchable avec son bouclier et décide de piocher !\n", bot.nom);
+    return 1; // 1 = piocher
+}
+    
     if (bot.nb_cartes_differentes >= 4) {
         printf(" L'ordinateur (%s) a reflechi et decide de s'arreter.\n", bot.nom);
         return 2; // 2 = s'arrêter
